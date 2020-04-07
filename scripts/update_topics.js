11541cd52cc722854aedb938d2f64a2333f21bbc
rox <aaronf@gmail.com>
2020-04-06 19:06:39 -0500

    add script to add topic data to index

---
+++ scripts/update_topics.js
@@ -0,0 +1,119 @@
+var elasticsearch = require('@elastic/elasticsearch');
+var lda = require('lda');
+var stopwords = require('lda/lib/stopwords_en.js');
+elasticClient = new elasticsearch.Client({
+    node: 'http://localhost:9200',
+    log: 'info'
+});
+
+var q = {
+  "version": true,
+  "size": 500,
+  "sort": [
+    {
+      "visited": {
+        "order": "desc",
+        "unmapped_type": "boolean"
+      }
+    }
+  ],
+  "_source": {
+    "excludes": []
+  },
+  "stored_fields": [
+    "*"
+  ],
+  //"docvalue_fields": [
+  //  {
+  //    "field": "visited",
+  //    "format": "date_time"
+  //  }
+  //],
+  "query": {
+    "bool": {
+      "must": [],
+      "filter": [
+        {
+          "match_all": {}
+        }
+      ],
+      "should": [],
+      "must_not": [
+        { "exists": { "field": "topic" }},
+        { "term": { "category": "personal" }},
+      ]
+    }
+  }
+}
+
+elasticClient.search({
+  index: 'urls',
+  body: q
+})
+.catch(console.log)
+.then(function(res) {
+  res.body.hits.hits.forEach(function(doc) {
+    if (doc._source.content == '' || doc._source.content == null) {
+      return;
+    }
+    if (doc._source.category.includes('personal') || doc._source.category.includes('adult')) {
+      return;
+    }
+    if (doc._source.category.includes('search')) {
+      if (doc._source.domain.includes('google.com')) {
+        topics = []
+        doc._source.query.split('&').forEach(function(q) {
+          if (q.startsWith('q=')) {
+            q.split('=')[1].split('+').forEach(function(word) {
+              if (!stopwords.stop_words.includes(q)) topics.push(word)
+            });
+          }
+        });
+        console.log(doc._source.link);
+        console.log(topics);
+        elasticClient.update({
+          index: 'urls',
+          id: doc._id,
+          body: {
+            doc: {
+              "topic": topics
+            }
+          }
+        })
+        .catch(console.log);
+      }
+      return;
+    }
+
+    content = doc._source.content[0];
+    //content = content.replace(/[’']([st]) /g, "$1 "); 
+    //content = content.replace(/[“”‘]+/g, '"'); 
+    //content = content.replace(/[\(\)\[\],]/g, ''); 
+    content = content.replace(/\S+:\/\/\S+/g, ''); 
+    content = content.replace(/\S+\.com\/\S*/g, ''); 
+    //console.log(content);
+    var s = content.match( /[^\.!\?]+[\.!\? ]+/g );
+    var l = lda(s, 2, 5);
+    console.log(doc._id);
+    console.log(doc._source.link);
+    //console.log(l);
+    topics = [];
+    for (topic in l) {
+      for (term in l[topic]) {
+        topics.push(l[topic][term]['term'].replace(/['"“”‘’]/, ''));
+      }
+    }
+    console.log(topics);
+
+    elasticClient.update({
+      index: 'urls',
+      id: doc._id,
+      body: {
+        doc: {
+          "topic": topics
+        }
+      }
+    })
+    .catch(console.log);
+  })
+})
