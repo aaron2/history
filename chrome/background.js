a0468f98dedbd31cccad5c7407c75a57cbfc3ad3
rox <aaronf@gmail.com>
2020-04-26 14:59:44 -0500

    improve text extraction. add menu for setting page significance

--- chrome/background.js
+++ chrome/background.js
@@ -6,17 +6,19 @@ chrome.webNavigation.onCommitted.addListener(function(completedDetails) {
     return;
   }
   chrome.tabs.get(completedDetails.tabId, function(tab) {
-    chrome.tabs.executeScript(completedDetails.tabId, { file: "gettext.js" }, function(text) {
+    chrome.tabs.executeScript(completedDetails.tabId, { file: "gettext.js" }, function(pagedata) {
       fetch("https://_YOUR_HOST_HERE_/history/add", {
         method: 'post',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
           url: tab.url,
-          title: tab.title,
-          text: text,
+          title: pagedata[0].title,
+          text: pagedata[0].text,
+          referrer: pagedata[0].referrer,
           transition_type: completedDetails.transitionType,
         }),
       });
     });
   });
 });
+
--- chrome/gettext.js
+++ chrome/gettext.js
@@ -1,3 +1,11 @@
+function getPageData() {
+  return {
+    referrer: document.referrer,
+    title: document.title,
+    text: getText(document.body)
+  };
+}
+
 function getText(el) {
     return walkNodeTree(el, {
         inspect: n => !['STYLE', 'SCRIPT', 'SVG', 'IFRAME', 'NOSCRIPT'].includes(n.nodeName.toUpperCase()),
@@ -26,12 +34,35 @@ function walkNodeTree(root, options) {
     let n;
     while(n = walker.nextNode()) {
         s = n.textContent.trim();
-        if (s !== "") {
-          text += ' '+s;
+        if (ignore(s)) {
+          continue;
         }
+        //console.log(s.length, s);
+        text += ' '+s;
     }
-
+    //console.log(text);
     return text;
 }
 
-getText(document.body);
+function ignore(s) {
+  if (s.length > 25) {
+    return false;
+  }
+  if (s.length <= 2) {
+    return true;
+  }
+
+  s = s.toLowerCase();
+
+  var ignore = ['facebook', 'twitter', 'pocket', 'linkedin', 'reddit', 'login', 'log in', 'sign up', 'social', 'share', 'sharing', 'comment', 'cookie', 'privacy', 'skip', 'new window', 'terms', 'search'];
+
+  for (term in ignore) {
+    if (s.includes(ignore[term])) {
+      //console.log('ignore', s);
+      return true;
+    }
+  }
+  return false;
+}
+
+getPageData();
